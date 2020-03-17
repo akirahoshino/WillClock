41ffc252ceaae053d10a56154ca8fbe597934bd7
akirahoshino <awsosphic@gmail.com>
2020-03-17 12:04:49 +0000

    Installed yarn, webpack, devise

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module WillClock
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    # config.hosts << "21c024882b21437b8fe8cef0a4f30bd3.vfs.cloud9.us-east-1.amazonaws.com"
+    config.hosts << ".amazonaws.com"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
