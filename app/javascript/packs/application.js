9b614fad2b9b01cf35365bd34e103fad3cbdcd99
akirahoshino <awsosphic@gmail.com>
2020-04-07 00:11:25 +0000

    clean up around goals table

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,4 @@ require("@fortawesome/fontawesome-free");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import '../stylesheets/application'
