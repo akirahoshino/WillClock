a3c5869e1d35116c6d73a6c58e460d340899fbde
akirahoshino <awsosphic@gmail.com>
2020-04-07 20:41:34 +0000

    countdown timer, cannot specify which goal

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,25 @@
-const { environment } = require('@rails/webpacker')
-var webpack = require('webpack');
+// const { environment } = require('@rails/webpacker')
+// var webpack = require('webpack');
 
-environment.plugins.append(
-  'Provide',
+// environment.plugins.append(
+//   'Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery',
+//   })
+// )
+
+// module.exports = environment
+
+
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+    noUiSlider: 'nouislider'
   })
 )
 
