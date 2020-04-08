f21556b142e4f38034ae35147856358317bea5b8
akirahoshino <awsosphic@gmail.com>
2020-04-08 17:26:18 +0000

    task index w/table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,43 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params(params))
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+    @task.assign_attributes(task_params(params))
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    current_user.tasks.find(params[:id]).destroy
+    redirect_to tasks_path
+  end
+  
+  private
+  
+  def task_params(params)
+    params.require(:task).permit(:info)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   
   # resources :goals
   resources :goals #, only: [:index, :new, :create, :edit, :update]
+  resources :tasks #, only: [:index, :new, :create, :edit, :update]
   #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
