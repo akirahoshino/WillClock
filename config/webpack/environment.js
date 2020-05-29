260f70e00cb6a1d19686668cf7726d22ca579be3
akirahoshino <awsosphic@gmail.com>
2020-05-29 11:57:46 +0000

    Delete: comments

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
-// const { environment } = require('@rails/webpacker')
-// var webpack = require('webpack');
-
-// environment.plugins.append(
-//   'Provide',
-//   new webpack.ProvidePlugin({
-//     $: 'jquery',
-//   })
-// )
-
-// module.exports = environment
-
-
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
